120e9224e2de2f066645b061837cdb00384ebe4a
Drake <drakesmurphy@gmail.com>
2025-05-15 10:30:20 -0400

    simple grenade system

--- src/shoot.rs
+++ src/shoot.rs
@@ -1,4 +1,4 @@
-use crate::{DamageEvent, Player};
+use crate::{DamageEvent, Player, PlayerCamera};
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
@@ -7,12 +7,24 @@ use super::GameState;
 #[derive(Event)]
 pub struct ShootEvent;
 
+#[derive(Debug, Component)]
+pub struct Grenade; /*{
+    rigid_body: RigidBody,
+    transform: Transform,
+    restitution: Restitution,
+    damage: i32,
+    splash_radius: i32,
+    timer: Timer,
+    collider: Collider,
+}*/
+
 pub struct ShootPlugin;
 
 impl Plugin for ShootPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<ShootEvent>()
-            .add_systems(Update, shoot.run_if(in_state(GameState::Gameplay)));
+            //.add_systems(Update, shoot.run_if(in_state(GameState::Gameplay)));
+            .add_systems(Update, shoot_grenade.run_if(in_state(GameState::Gameplay)));
     }
 }
 
@@ -21,12 +33,12 @@ pub fn shoot(
     mut damage_event: EventWriter<DamageEvent>,
     ray_caster: SpatialQuery,
     player: Query<Entity, With<Player>>,
-    query: Query<(&Camera, &GlobalTransform)>,
+        query: Query<&GlobalTransform, With<Camera>>,
 ) {
     trace!("Event Handler: shoot");
     let player = player.get_single().unwrap();
     for _event in shoot_events.read() {
-        for (_camera, global_transform) in query.iter() {
+        for global_transform in query.iter() {
             let camera_position = global_transform.translation();
             let direction = global_transform.forward();
 
@@ -47,6 +59,43 @@ pub fn shoot(
                     ammount: 10,
                 });
             }
+        }
+    }
+}
+
+const GRENADE_SIZE: f32 = 0.1;
+
+pub fn shoot_grenade(
+    mut commands: Commands,
+    mut shoot_events: EventReader<ShootEvent>,
+    mut damage_event: EventWriter<DamageEvent>,
+    player_entity_query: Query<Entity, With<Player>>,
+    camera_transform_query: Query<&GlobalTransform, With<PlayerCamera>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    trace!("Event Handler: shoot_grenade");
+    for _event in shoot_events.read() {
+        for global_transform in camera_transform_query.iter() {
+            let camera_position = global_transform.translation();
+            println!("Camera Position: {}", camera_position);
+            let grenade_position = camera_position + (global_transform.forward() * 3.);
+            println!("Grenade Position: {}", grenade_position);
+            let direction = global_transform.forward();
+            let linear_velocity = direction * 23.;
+            commands.spawn((
+                    Grenade,
+                    RigidBody::Dynamic,
+                    Collider::capsule(GRENADE_SIZE, GRENADE_SIZE),
+                    Mesh3d(meshes.add(Capsule3d::new(GRENADE_SIZE, GRENADE_SIZE))),
+                    MeshMaterial3d(materials.add(Color::WHITE)),
+                    Transform {
+                        translation: grenade_position,
+                        ..default()
+                    },
+                    LinearVelocity(linear_velocity),
+                    Restitution::new(1.),
+            ));
         }
     }
 }
