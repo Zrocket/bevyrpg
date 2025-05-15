ae54eb85c4f983a819dc781db6f53e5b67430efb
Drake <drakesmurphy@gmail.com>
2025-05-15 19:06:09 -0400

    grenades now explode, and addded rockets

--- src/shoot.rs
+++ src/shoot.rs
@@ -1,21 +1,27 @@
+use std::time::Instant;
+
 use crate::{DamageEvent, Player, PlayerCamera};
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
 use super::GameState;
 
+const GRENADE_SIZE: f32 = 0.1;
+const ROCKET_SIZE: f32 = 0.1;
+
 #[derive(Event)]
 pub struct ShootEvent;
 
 #[derive(Debug, Component)]
-pub struct Grenade; /*{
-    rigid_body: RigidBody,
-    transform: Transform,
-    restitution: Restitution,
+pub struct Grenade(Timer); /*{
     damage: i32,
     splash_radius: i32,
-    timer: Timer,
-    collider: Collider,
+}*/
+
+#[derive(Debug, Component)]
+pub struct Rocket; /*{
+    damage: i32,
+    splash_radius: i32,
 }*/
 
 pub struct ShootPlugin;
@@ -24,7 +30,12 @@ impl Plugin for ShootPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<ShootEvent>()
             //.add_systems(Update, shoot.run_if(in_state(GameState::Gameplay)));
-            .add_systems(Update, shoot_grenade.run_if(in_state(GameState::Gameplay)));
+            .add_systems(Update, (
+                //shoot_grenade.run_if(in_state(GameState::Gameplay)),
+                shoot_rocket.run_if(in_state(GameState::Gameplay)),
+                grenade_fuse.run_if(in_state(GameState::Gameplay)),
+                ),
+            );
     }
 }
 
@@ -63,9 +74,51 @@ pub fn shoot(
     }
 }
 
-const GRENADE_SIZE: f32 = 0.1;
+pub fn grenade_fuse(
+    mut commands: Commands,
+    mut grenade_query: Query<(Entity, &mut Grenade)>,
+    time: Res<Time>,
+) {
+    for (entity, mut grenade) in grenade_query.iter_mut() {
+        grenade.0.tick(time.delta());
+
+        if grenade.0.finished() {
+            commands.entity(entity).despawn();
+        }
+    }
+}
 
 pub fn shoot_grenade(
+    mut commands: Commands,
+    mut shoot_events: EventReader<ShootEvent>,
+    camera_transform_query: Query<&GlobalTransform, With<PlayerCamera>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    trace!("Event Handler: shoot_grenade");
+    for _event in shoot_events.read() {
+        for global_transform in camera_transform_query.iter() {
+            let camera_position = global_transform.translation();
+            let grenade_position = camera_position + (global_transform.forward() * 3.);
+            let direction = global_transform.forward();
+            let linear_velocity = direction * 23.;
+            commands.spawn((
+                    Grenade(Timer::from_seconds(3., TimerMode::Once)),
+                    RigidBody::Dynamic,
+                    Collider::capsule(GRENADE_SIZE, GRENADE_SIZE),
+                    Mesh3d(meshes.add(Capsule3d::new(GRENADE_SIZE, GRENADE_SIZE))),
+                    MeshMaterial3d(materials.add(Color::WHITE)),
+                    Transform {
+                        translation: grenade_position,
+                        ..default()
+                    },
+                    LinearVelocity(linear_velocity),
+            ));
+        }
+    }
+}
+
+pub fn shoot_rocket(
     mut commands: Commands,
     mut shoot_events: EventReader<ShootEvent>,
     mut damage_event: EventWriter<DamageEvent>,
@@ -78,24 +131,27 @@ pub fn shoot_grenade(
     for _event in shoot_events.read() {
         for global_transform in camera_transform_query.iter() {
             let camera_position = global_transform.translation();
-            println!("Camera Position: {}", camera_position);
-            let grenade_position = camera_position + (global_transform.forward() * 3.);
-            println!("Grenade Position: {}", grenade_position);
+            let rocket_position = camera_position + (global_transform.forward() * 3.);
             let direction = global_transform.forward();
-            let linear_velocity = direction * 23.;
+            let linear_velocity = direction * 20.;
             commands.spawn((
-                    Grenade,
-                    RigidBody::Dynamic,
-                    Collider::capsule(GRENADE_SIZE, GRENADE_SIZE),
-                    Mesh3d(meshes.add(Capsule3d::new(GRENADE_SIZE, GRENADE_SIZE))),
+                    Rocket,
+                    RigidBody::Kinematic,
+                    Collider::capsule(ROCKET_SIZE, ROCKET_SIZE),
+                    Mesh3d(meshes.add(Capsule3d::new(ROCKET_SIZE, ROCKET_SIZE))),
                     MeshMaterial3d(materials.add(Color::WHITE)),
                     Transform {
-                        translation: grenade_position,
+                        translation: rocket_position,
                         ..default()
                     },
                     LinearVelocity(linear_velocity),
-                    Restitution::new(1.),
             ));
         }
     }
+}
+
+pub fn explode_rocket(
+    mut commands: Commands,
+    mut rocket_query: Query<Entity, With<Rocket>>,
+) {
 }
