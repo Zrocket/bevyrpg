c8d484effc36f7d11f9e8537b17f0fb8c1e2035d
Drake <drakesmurphy@gmail.com>
2025-05-20 21:00:12 -0400

    added soft_ratatui

--- src/devroom.rs
+++ src/devroom.rs
@@ -1,9 +1,13 @@
 use avian3d::collision::collider::Collider;
 use bevy::asset::RenderAssetUsages;
-use bevy::color::palettes::css::{RED};
+use bevy::color::palettes::css::{GOLD, RED};
+use bevy::ecs::system::command::init_resource;
+use bevy::log::tracing_subscriber::fmt::format;
+use bevy::render::camera::RenderTarget;
 use bevy::render::render_resource::{Extent3d, TextureFormat, TextureUsages};
 use bevy::render::view::RenderLayers;
 use bevy::sprite::Material2d;
+use bevy::text::DEFAULT_FONT_DATA;
 use bevy::{
     core_pipeline::core_3d::Camera3d, math::vec3, prelude::*, render::camera::ClearColorConfig,
 };
@@ -12,6 +16,10 @@ use bevy_tnua::control_helpers::TnuaSimpleAirActionsCounter;
 use bevy_tnua::prelude::*;
 use bevy_tnua_avian3d::*;
 use leafwing_input_manager::{InputManagerBundle, input_map::InputMap};
+use ratatui::style::Stylize;
+use ratatui::widgets::{Block, Borders, Paragraph, Wrap};
+use ratatui::Terminal;
+use soft_ratatui::SoftBackend;
 
 use std::f32::consts::PI;
 
@@ -20,6 +28,19 @@ use super::RenderPlayer;
 use super::controller::*;
 
 use crate::*;
+
+static FONT_DATA: &[u8] = include_bytes!("../assets/iosevka.ttf");
+
+// Create resource to hold the ratatui terminal
+#[derive(Resource, Deref, DerefMut)]
+struct SoftTerminal(Terminal<SoftBackend>);
+impl Default for SoftTerminal {
+    fn default() -> Self {
+       let mut backend = SoftBackend::new_with_font(15, 15, 16, FONT_DATA);
+       //backend.set_font_size(12);
+       Self(Terminal::new(backend).unwrap())
+    }
+}
 
 #[derive(Debug, PhysicsLayer, Default, Component, Reflect)]
 #[reflect(Component)]
@@ -50,7 +71,8 @@ impl Plugin for DevRoomPlugin {
                     spawn_player,
                     spawn_walking_cube,
                     spawn_sphere,
-                    spawn_projection_cube
+                    //spawn_projection_cube
+                    spawn_terminal,
                 ).chain()
             )
             .register_type::<CollisionLayer>()
@@ -58,7 +80,8 @@ impl Plugin for DevRoomPlugin {
             .register_type::<MainPassCube>()
             .add_plugins(AtmospherePlugin)
             .add_systems(Update, player_forward.run_if(in_state(GameState::Gameplay)))
-            .add_systems(Update, rotator_system);
+            .add_systems(Update, rotator_system)
+            .init_resource::<SoftTerminal>();
 
             //.add_systems(OnEnter(GameState::Gameplay), spawn_sprites)
             //.add_plugins(SpritesPlugin);
@@ -497,6 +520,101 @@ fn spawn_projection_cube(
     )
 } */
 
+fn spawn_terminal(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut softatui: ResMut<SoftTerminal>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    softatui
+        .draw(|frame| {
+            let area = frame.area();
+            let textik = format!("Hello bevy! The window area is {}", area);
+            frame.render_widget(
+                Paragraph::new(textik)
+                    .block(Block::new().title("Ratatui").borders(Borders::ALL))
+                    .white()
+                    .on_blue()
+                    .wrap(Wrap { trim: false }),
+                area,
+            );
+        })
+        .expect("epic fail");
+
+    let width = softatui.backend().get_pixmap_width() as u32;
+    let height = softatui.backend().get_pixmap_height() as u32;
+    let data = softatui.backend().get_pixmap_data_as_rgba();
+
+    let mut image = Image::new(
+        Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        },
+        bevy::render::render_resource::TextureDimension::D2,
+        data,
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+    );
+    // You need to set these texture usage flags in order to use the image as a render target
+    image.texture_descriptor.usage =
+        TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST | TextureUsages::RENDER_ATTACHMENT;
+
+    let image_handle = images.add(image);
+
+    // Light
+    commands.spawn(DirectionalLight::default());
+
+    let texture_camera = commands
+        .spawn((
+            Camera2d,
+            Camera {
+                target: RenderTarget::Image(image_handle.clone().into()),
+                ..default()
+            },
+        ))
+        .id();
+
+    commands
+        .spawn((
+            Node {
+                // Cover the whole image
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BackgroundColor(GOLD.into()),
+            UiTargetCamera(texture_camera),
+        ))
+        .with_children(|parent| {
+            parent.spawn(ImageNode::new(image_handle.clone()));
+        });
+
+    let cube_size = 4.0;
+    let cube_handle = meshes.add(Cuboid::new(cube_size, cube_size, cube_size));
+
+    // This material has the texture that has been rendered.
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(image_handle),
+        reflectance: 0.02,
+        unlit: false,
+
+        ..default()
+    });
+
+    // Cube with material containing the rendered UI texture.
+    commands.spawn((
+        Mesh3d(cube_handle),
+        MeshMaterial3d(material_handle),
+        Transform::from_xyz(0.0, 0.0, 1.5).with_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        FirstPassCube,
+    ));
+}
+
 fn player_forward(
     cam_transform: Query<&Transform, (With<PlayerCamera>, Without<Player>)>,
     mut player_transform: Query<&mut Transform, With<Player>>,
@@ -516,8 +634,8 @@ fn rotator_system(
     mut query: Query<&mut Transform, With<FirstPassCube>>,
 ) {
     for mut transform in &mut query {
-        transform.rotate_x(1.5 * time.delta_secs());
-        transform.rotate_z(1.5 * time.delta_secs());
+        transform.rotate_x(1.0 * time.delta_secs());
+        transform.rotate_y(0.7 * time.delta_secs());
     }
 }
 
