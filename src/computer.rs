bbeda321d05a0a75587951a4e7f260edb4291b22
Drake <drakesmurphy@gmail.com>
2025-05-23 17:44:54 -0400

    moved computer to new file

--- src/computer.rs
+++ src/computer.rs
@@ -1,6 +1,15 @@
 use bevy::prelude::*;
+use bevy::render::render_resource::{Extent3d, TextureFormat, TextureUsages};
+use bevy::color::palettes::css::GOLD;
+use ratatui::style::Stylize;
+use ratatui::widgets::{Block, Borders, Paragraph, Wrap};
+use ratatui::{Frame, Terminal};
+use soft_ratatui::SoftBackend;
+use bevy::asset::RenderAssetUsages;
+use bevy::render::camera::RenderTarget;
+use std::f32::consts::PI;
 
-use crate::Player;
+use crate::GameState;
 
 #[derive(Event)]
 pub struct UseComputerEvent {
@@ -10,7 +19,149 @@ pub struct UseComputerEvent {
 pub struct ComputerPlugin;
 
 impl Plugin for ComputerPlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.init_resource::<SoftTerminal>()
+            .register_type::<TerminalCube>()
+            .register_type::<ComputerTextureCam>()
+            .add_systems(OnEnter(GameState::Loading), spawn_terminal);
+    }
 }
 
-fn _use_computer_event_handler(_player_query: Query<Entity, With<Player>>) {}
+static FONT_DATA: &[u8] = include_bytes!("../assets/iosevka.ttf");
+
+#[derive(Debug, Clone, Component, Reflect)]
+#[reflect(Component)]
+pub struct TerminalCube;
+
+#[derive(Debug, Clone, Component, Reflect)]
+#[reflect(Component)]
+pub struct ComputerTextureCam;
+
+#[derive(Resource)]
+pub struct MyProcGenImage(pub Handle<Image>);
+
+// Create resource to hold the ratatui terminal
+#[derive(Resource, Deref, DerefMut)]
+pub struct SoftTerminal(Terminal<SoftBackend>);
+impl Default for SoftTerminal {
+    fn default() -> Self {
+       let backend = SoftBackend::new_with_font(15, 15, 12, FONT_DATA);
+       //backend.set_font_size(12);
+       Self(Terminal::new(backend).unwrap())
+    }
+}
+
+fn spawn_terminal(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut softatui: ResMut<SoftTerminal>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    softatui
+        .draw(draw_computer_screen)
+        .expect("epic fail");
+
+    let width = softatui.backend().get_pixmap_width() as u32;
+    let height = softatui.backend().get_pixmap_height() as u32;
+    let data = softatui.backend().get_pixmap_data_as_rgba();
+
+    let mut image = Image::new(
+        Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        },
+        bevy::render::render_resource::TextureDimension::D2,
+        data,
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+    );
+    // You need to set these texture usage flags in order to use the image as a render target
+    image.texture_descriptor.usage =
+        TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST | TextureUsages::RENDER_ATTACHMENT;
+
+    let image_handle = images.add(image);
+    commands.insert_resource(MyProcGenImage(image_handle.clone()));
+
+    // Light
+    commands.spawn(DirectionalLight::default());
+
+    let texture_camera = commands
+        .spawn((
+            Camera2d,
+            Camera {
+                target: RenderTarget::Image(image_handle.clone().into()),
+                ..default()
+            },
+            ComputerTextureCam,
+            Name::new("ComputerTextureCam"),
+        ))
+        .id();
+
+    commands
+        .spawn((
+            Node {
+                // Cover the whole image
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BackgroundColor(GOLD.into()),
+            UiTargetCamera(texture_camera),
+            Name::new("ComputerBox"),
+        ))
+        .with_children(|parent| {
+            parent.spawn(ImageNode::new(image_handle.clone()));
+        });
+
+    let cube_size = 4.0;
+    let cube_handle = meshes.add(Cuboid::new(cube_size, cube_size, cube_size));
+
+    // This material has the texture that has been rendered.
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(image_handle),
+        reflectance: 0.02,
+        unlit: false,
+
+        ..default()
+    });
+
+    // Cube with material containing the rendered UI texture.
+    commands.spawn((
+        Mesh3d(cube_handle),
+        MeshMaterial3d(material_handle),
+        Transform::from_xyz(15.0, 2.0, 1.5).with_rotation(Quat::from_rotation_y(PI)),
+        TerminalCube,
+        Name::new("TerminalCube"),
+    ));
+}
+
+pub fn draw_computer_screen(frame: &mut Frame) {
+    let area = frame.area();
+    let textik = format!("Hello bevy! The window area is {}", area);
+    frame.render_widget(
+        Paragraph::new(textik)
+            .block(Block::new().title("Ratatui").borders(Borders::ALL))
+            .white()
+            .on_blue()
+            .wrap(Wrap { trim: false }),
+        area,
+    );
+}
+
+pub fn new_computer_screen(frame: &mut Frame) {
+    let area = frame.area();
+    let textik = format!("Hello bevy! The window area is {}", area);
+    frame.render_widget(
+        Paragraph::new(textik)
+            .block(Block::new().title("Ratatui").borders(Borders::ALL))
+            .white()
+            .on_red()
+            .wrap(Wrap { trim: false }),
+        area,
+    );
+}
