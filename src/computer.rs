cf28603c9724c79ccbcf9fffc2a5950e3c8f767b
Drake <fyiohhai@gmail.com>
2025-05-28 13:09:08 -0400

    Made Computer Cube interactable

--- src/computer.rs
+++ src/computer.rs
@@ -1,6 +1,8 @@
+use avian3d::prelude::{Collider, RigidBody};
 use bevy::prelude::*;
 use bevy::render::render_resource::{Extent3d, TextureFormat, TextureUsages};
 use bevy::color::palettes::css::GOLD;
+use bevy_trait_query::RegisterExt;
 use ratatui::style::Stylize;
 use ratatui::widgets::{Block, Borders, Paragraph, Wrap};
 use ratatui::{Frame, Terminal};
@@ -9,7 +11,15 @@ use bevy::asset::RenderAssetUsages;
 use bevy::render::camera::RenderTarget;
 use std::f32::consts::PI;
 
+use crate::interact::Interaction;
 use crate::GameState;
+
+#[derive(Clone, Hash, Debug, Eq, PartialEq, Default, States)]
+pub enum ComputerState {
+    #[default]
+    MainMenu,
+    Console,
+}
 
 #[derive(Event)]
 pub struct UseComputerEvent {
@@ -21,9 +31,13 @@ pub struct ComputerPlugin;
 impl Plugin for ComputerPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<SoftTerminal>()
-            .register_type::<TerminalCube>()
+            .register_type::<ComputerCube>()
+            .register_component_as::<dyn Interaction, ComputerCube>()
             .register_type::<ComputerTextureCam>()
-            .add_systems(OnEnter(GameState::Loading), spawn_terminal);
+            .add_event::<ChangeScreenEvent<>>()
+            .add_observer(change_computer_screen)
+            //.add_systems(Update, change_computer_screen)
+            .add_systems(OnEnter(GameState::Loading), spawn_computer);
     }
 }
 
@@ -31,7 +45,12 @@ static FONT_DATA: &[u8] = include_bytes!("../assets/iosevka.ttf");
 
 #[derive(Debug, Clone, Component, Reflect)]
 #[reflect(Component)]
-pub struct TerminalCube;
+pub struct ComputerCube;
+impl Interaction for ComputerCube {
+    fn interact(&self,commands: &mut Commands,entity:Entity,prop:Entity,) {
+        commands.trigger_targets(ChangeScreenEvent{ frame_closure: new_computer_screen}, prop);
+    }
+}
 
 #[derive(Debug, Clone, Component, Reflect)]
 #[reflect(Component)]
@@ -39,6 +58,11 @@ pub struct ComputerTextureCam;
 
 #[derive(Resource)]
 pub struct MyProcGenMaterial(pub Handle<StandardMaterial>);
+
+#[derive(Event)]
+pub struct ChangeScreenEvent {
+    pub frame_closure: fn(&mut Frame),
+}
 
 // Create resource to hold the ratatui terminal
 #[derive(Resource, Deref, DerefMut)]
@@ -51,7 +75,47 @@ impl Default for SoftTerminal {
     }
 }
 
-fn spawn_terminal(
+fn change_computer_screen (
+    trigger: Trigger<ChangeScreenEvent>,
+    mut softatui: ResMut<SoftTerminal>,
+    proc_material: Res<MyProcGenMaterial>,
+    mut images: ResMut<Assets<Image>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    trace!("SYSTEM: computer_test");
+
+    softatui.draw(trigger.frame_closure.clone())
+        .expect("oops");
+
+    let width = softatui.backend().get_pixmap_width() as u32;
+    let height = softatui.backend().get_pixmap_height() as u32;
+    let data = softatui.backend().get_pixmap_data_as_rgba();
+    let material = materials
+        .get_mut(&proc_material.0)
+        .expect("material not found!");
+
+    let image = images
+        .get_mut(material.base_color_texture.as_ref().unwrap().id())
+        .expect("Image not found!");
+
+    let mut temp = Image::new(
+        Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        },
+        bevy::render::render_resource::TextureDimension::D2,
+        data,
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+    );
+    temp.texture_descriptor.usage = TextureUsages::TEXTURE_BINDING
+        | TextureUsages::COPY_DST
+        | TextureUsages::RENDER_ATTACHMENT;
+    *image = temp;
+}
+
+fn spawn_computer(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -136,8 +200,10 @@ fn spawn_terminal(
         Mesh3d(cube_handle),
         MeshMaterial3d(material_handle),
         Transform::from_xyz(15.0, 2.0, 1.5).with_rotation(Quat::from_rotation_y(PI)),
-        TerminalCube,
-        Name::new("TerminalCube"),
+        Collider::cuboid(4.0, 4.0, 4.0),
+        RigidBody::Static,
+        ComputerCube,
+        Name::new("ComputerCube"),
     ));
 }
 
