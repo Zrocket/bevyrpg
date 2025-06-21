303c0970641c3597cb4ae76cc468adeb71587c36
Drake <fyiohhai@gmail.com>
2025-06-21 10:35:10 -0400

    created and moved stuff to new furniture mod

---
+++ src/furniture/door.rs
@@ -0,0 +1,52 @@
+use bevy::prelude::*;
+use bevy_trait_query::RegisterExt;
+
+use crate::interact::Interaction;
+
+#[derive(Clone, Hash, Debug, Eq, PartialEq, Default)]
+pub enum DoorState {
+    #[default]
+    Closed,
+    Open,
+}
+
+#[derive(Resource)]
+pub struct DoorAnimation(pub Handle<AnimationClip>);
+#[derive(Resource)]
+pub struct DoorGraph(pub Handle<AnimationGraph>);
+
+#[derive(Event)]
+pub struct DoorEvent {
+    actor: Entity,
+    target: Entity,
+}
+
+#[derive(Debug, Default, Component, Reflect)]
+#[reflect(Component)]
+pub struct DoorComponent;
+impl Interaction for DoorComponent {
+    fn interact(&self,commands: &mut Commands,entity:Entity,prop:Entity,) {
+        println!("Door Interaction");
+        commands.trigger_targets(DoorEvent {actor: entity, target: prop}, entity);
+    }
+}
+
+pub struct DoorPlugin;
+impl Plugin for DoorPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<DoorComponent>()
+            .register_component_as::<dyn Interaction, DoorComponent>()
+            .add_event::<DoorEvent>()
+            .add_observer(door_event_observer);
+    }
+}
+
+fn door_event_observer(
+    trigger: Trigger<DoorEvent>,
+    mut door: Query<(Entity, &mut AnimationPlayer)>,
+) {
+    trace!("Door Event Handler");
+    if let Ok((door_entity, mut door_animation_player)) = door.single_mut() {
+        door_animation_player.play(1.into());
+    }
+}
