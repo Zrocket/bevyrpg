eb0eebfa5428bb0030310cbde16120fe7c15953b
Drake <fyiohhai@gmail.com>
2025-06-23 15:32:41 -0400

    created simple inspect interaction and moved interaction ray casting to dedicated function and component

--- src/interact.rs
+++ src/interact.rs
@@ -7,11 +7,26 @@ pub trait Interaction {
         commands: &mut Commands,
         entity: Entity,
         prop: Entity,
-        );
+    );
+}
+
+#[bevy_trait_query::queryable]
+pub trait Inspectable {
+    fn inspect( &self,
+        commands: &mut Commands,
+        entity: Entity,
+        prop: Entity,
+    );
 }
 
 #[derive(Event)]
 pub struct InteractEvent {
+    pub actor: Entity,
+    pub target: Entity,
+}
+
+#[derive(Event)]
+pub struct InspectEvent {
     pub actor: Entity,
     pub target: Entity,
 }
@@ -22,7 +37,8 @@ impl Plugin for InteractPlugin {
     fn build(&self, app: &mut App) {
         trace!("InteractPlugin build");
         app.add_plugins(AvianPickupPlugin::default())
-            .add_event::<InteractEvent>();
+            .add_event::<InteractEvent>()
+            .add_event::<InspectEvent>();
     }
 }
 
