3f832e31fab6fd35fc88dda87b9174eeeb280503
Drake <fyiohhai@gmail.com>
2025-06-24 17:49:53 -0400

    started work on ladders and levers

---
+++ src/furniture/lever.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+use bevy_trait_query::RegisterExt;
+
+use crate::interact::Interaction;
+
+#[derive(Event)]
+pub struct LeverEvent {
+    actor: Entity,
+    target: Entity,
+}
+
+#[derive(Debug, Default, Component, Reflect)]
+#[reflect(Component)]
+pub struct LeverComponent;
+impl Interaction for LeverComponent {
+    fn interact(&self,commands: &mut Commands,entity:Entity,prop:Entity,) {
+        println!("Lever Interaction");
+        commands.trigger_targets(LeverEvent {actor: entity, target: prop}, entity);
+    }
+}
+
+pub struct LeverPlugin;
+impl Plugin for LeverPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<LeverComponent>()
+            .register_component_as::<dyn Interaction, LeverComponent>()
+            .add_event::<LeverEvent>()
+            .add_observer(lever_event_observer);
+    }
+}
+
+fn lever_event_observer(
+    trigger: Trigger<LeverEvent>,
+    lever: Query<Entity, With<LeverComponent>>,
+) {
+    trace!("OBSERVER: lever_event_observer");
+    if let Ok(lever_entity) = lever.single() {
+    }
+}
