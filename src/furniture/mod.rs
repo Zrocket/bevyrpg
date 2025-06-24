b96045e5fb5fae7738fa96e0ad9a45fca2580b5c
Drake <fyiohhai@gmail.com>
2025-06-24 17:49:53 -0400

    started work on ladders and levers

--- src/furniture/mod.rs
+++ src/furniture/mod.rs
@@ -3,10 +3,14 @@ use bevy::prelude::*;
 mod chair;
 mod computer;
 mod door;
+mod ladder;
+mod lever;
 
 pub use chair::*;
 pub use computer::*;
 pub use door::*;
+pub use ladder::*;
+pub use lever::*;
 
 pub struct FurniturePlugin;
 impl Plugin for FurniturePlugin {
