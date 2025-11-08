af39587322d7e46fa0a9a2baada8520e8260c3bb
Drake <fyiohhai@gmail.com>
2025-11-08 15:35:38 -0500

    new ui systems

---
+++ src/ui/menu/controller_settings.rs
@@ -0,0 +1,50 @@
+use bevy::prelude::*;
+
+use crate::{PauseMenuState, widgets::{self, ui_root}};
+
+#[derive(Component, Reflect)]
+pub struct UiControllerSettings;
+
+pub struct ControllerSettingsMenuUiPlugin;
+impl Plugin for ControllerSettingsMenuUiPlugin {
+    fn build(&self, app: &mut bevy::app::App) {
+       app
+           .register_type::<UiControllerSettings>()
+           .add_systems(OnEnter(PauseMenuState::ControllerSettings), spawn_controller_settings_menu);
+    }
+}
+
+fn spawn_controller_settings_menu(
+    mut commands: Commands,
+) {
+    commands.spawn((
+            ui_root("Controller Settings"),
+            DespawnOnExit(PauseMenuState::ControllerSettings),
+            GlobalZIndex(2),
+            UiControllerSettings,
+            children![
+            (
+                widgets::label("Mouse Sensetivity"),
+                Node {
+                    justify_self: JustifySelf::End,
+                    ..default()
+                },
+            ),
+            (
+                widgets::label("Key Bindings"),
+                Node {
+                    justify_self: JustifySelf::End,
+                    ..default()
+                },
+            ),
+                widgets::button("Back", back_to_settings_menu),
+            ],
+    ));
+}
+
+fn back_to_settings_menu(
+    _: On<Pointer<Click>>,
+    mut pause_menu_state: ResMut<NextState<PauseMenuState>>,
+) {
+    pause_menu_state.set(PauseMenuState::Settings);
+}
