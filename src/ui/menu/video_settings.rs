dad536a92d6fd3b26f8c6efa36d22fec782680d3
Drake <fyiohhai@gmail.com>
2025-11-08 15:35:38 -0500

    new ui systems

---
+++ src/ui/menu/video_settings.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+
+use crate::{PauseMenuState,  widgets::{self, ui_root}};
+
+#[derive(Component, Reflect)]
+pub struct UiVideoSettings;
+
+pub struct VideoSettingsMenuUiPlugin;
+impl Plugin for VideoSettingsMenuUiPlugin {
+    fn build(&self, app: &mut bevy::app::App) {
+       app
+           .register_type::<UiVideoSettings>()
+           .add_systems(OnEnter(PauseMenuState::VideoSettings), spawn_video_settings_menu);
+    }
+}
+
+fn spawn_video_settings_menu(
+    mut commands: Commands,
+) {
+    commands.spawn((
+            ui_root("Video Settings"),
+            DespawnOnExit(PauseMenuState::VideoSettings),
+            GlobalZIndex(2),
+            UiVideoSettings,
+            children![
+            (
+                widgets::label("Camera Sensetivity"),
+                Node {
+                    justify_self: JustifySelf::End,
+                    ..default()
+                },
+            ),
+                widgets::button("Back", back_to_settings_menu),
+            ],
+    ));
+}
+
+fn back_to_settings_menu(
+    _: On<Pointer<Click>>,
+    mut pause_menu_state: ResMut<NextState<PauseMenuState>>,
+) {
+    pause_menu_state.set(PauseMenuState::Settings);
+}
