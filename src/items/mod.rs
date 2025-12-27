3a32c126ec696ce43d00d692181d7ea72b1ed1cb
Drake <fyiohhai@gmail.com>
2025-12-27 15:21:56 -0500

    basic plug/socket test

--- src/items/mod.rs
+++ src/items/mod.rs
@@ -1,7 +1,6 @@
 mod ammo;
 mod armor;
 mod books;
-mod consume;
 mod container;
 mod misc;
 mod weapons;
@@ -11,20 +10,19 @@ use std::iter;
 pub use ammo::*;
 pub use armor::*;
 use avian_pickup::prop::HeldProp;
-use avian3d::prelude::CollisionLayers;
+use avian3d::prelude::{CollisionLayers, CollisionStart, Position, RigidBodyDisabled, Rotation};
 pub use books::*;
-pub use consume::*;
 pub use container::*;
 pub use misc::*;
 pub use weapons::*;
 
-use bevy::{ecs::system::IntoObserverSystem, prelude::*};
+use bevy::prelude::*;
 
 use crate::{level::CollisionLayer};
 
 #[derive(Component, Reflect, Clone, Default)]
 #[reflect(Component)]
-pub struct UnregisteredItem;
+pub struct RegisteredItem;
 
 #[derive(Component, Reflect, Clone, Default)]
 #[reflect(Component)]
@@ -39,6 +37,23 @@ pub struct ItemInteractionEvent {
     entity: Entity,
 }
 
+#[derive(Component, Reflect, Clone, Default)]
+#[reflect(Component)]
+pub struct SocketItem;
+
+#[derive(Component, Reflect, Clone, Default)]
+#[reflect(Component)]
+pub struct PlugItem;
+
+#[derive(Component, Reflect, Clone, Default)]
+#[reflect(Component)]
+pub struct MountPoint;
+
+#[derive(EntityEvent)]
+pub struct PlugSocketEvent {
+    entity: Entity,
+}
+
 #[derive(Component, Clone, Default)]
 pub struct Item {
     pub description: Description,
@@ -49,6 +64,9 @@ pub struct ItemPlugin;
 impl Plugin for ItemPlugin {
     fn build(&self, app: &mut App) {
         app
+            .register_type::<SocketItem>()
+            .register_type::<PlugItem>()
+            .register_type::<MountPoint>()
             .add_plugins((
    //                 AmmoPlugin,
   //                  ArmorPlugin,
@@ -58,27 +76,10 @@ impl Plugin for ItemPlugin {
 //                    WeaponPlugin,
             ))
             .add_observer(disabled_held_prop_collision)
-            .add_observer(enable_dropped_prop_collision);
+            .add_observer(enable_dropped_prop_collision)
+            .add_systems(Update, register_socket_items);
     }
 }
-
-/*fn register_items<E, B, M, I>(
-    mut commands: Commands,
-    observer_system: I,
-    mut unregistered_items_query: Query<Entity, Without<Interactable>>,
-)
-where
-    E: EntityEvent,
-    B: Component,
-    I: IntoObserverSystem<E, B, M>,
-{
-    let observer_system = IntoObserverSystem::into_system(observer_system);
-    let mut unregistered_items_query = unregistered_items_query.transmute_lens_filtered::<Entity, (Without<Interactable>, With<B>)>();
-    for unregistered_item in unregistered_items_query.query().iter_mut() {
-        commands.entity(unregistered_item).observe(observer_system)
-            .insert(Interactable);
-    }
-}*/
 
 fn disabled_held_prop_collision(
     add: On<Add, HeldProp>,
@@ -105,5 +106,30 @@ fn enable_dropped_prop_collision(
             continue;
         };
         collision_layers.filters.add(CollisionLayer::Player);
+    }
+}
+
+fn register_socket_items(
+    mut commands: Commands,
+    mut socket_query: Query<Entity, (With<SocketItem>, Without<RegisteredItem>)>
+) {
+    for socket in socket_query.iter_mut() {
+        commands.entity(socket)
+            .observe(socket_test)
+            .insert(RegisteredItem);
+    }
+}
+
+fn socket_test(
+    trigger: On<CollisionStart>,
+    mut commands: Commands,
+    mut plug_query: Query<(Entity, &mut Position, &mut Rotation), With<PlugItem>>,
+    mount_query: Query<(&Position, &Rotation), (With<MountPoint>, Without<PlugItem>)>,
+) {
+    if let Ok((plug_entity, mut plug_position, mut plug_rotation)) = plug_query.get_mut(trigger.event().collider2)
+    && let Ok((mount_position, mount_rotation)) = mount_query.single() {
+        *plug_position = mount_position.clone();
+        *plug_rotation = mount_rotation.clone();
+        commands.entity(plug_entity).insert(RigidBodyDisabled);
     }
 }
