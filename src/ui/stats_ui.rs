ee6e36f4fc008b2d5689d259d68a154f77b3df83
Drake <fyiohhai@gmail.com>
2026-01-10 20:53:27 -0500

    working stats ui

--- src/ui/stats_ui.rs
+++ src/ui/stats_ui.rs
@@ -29,11 +29,59 @@ pub fn spawn_stats_ui(
 pub fn display_stats_event_observer(
     trigger: On<DisplayStatsEvent>,
     mut commands: Commands,
-    player_query: Query<(&Level, &Experience, &Speed, &Corruption, &Matter, &Mind, &Luck, &Maneuver, &Hunger), With<Player>>
+    player_query: Query<(&Level, &Experience, &Speed, &Corruption, &Matter, &Mind, &Luck, &Maneuver), With<Player>>
 ) {
-    if let Ok((level, experience, speed, corruption, matter, mind, luck, maneuver, hunger)) = player_query.single() {
+    if let Ok((level, experience, speed, corruption, matter, mind, luck, maneuver)) = player_query.single() {
+        let level_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Level: {}", level.0)),
+        )).id();
+        let experience_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Experience: {}", experience.0)),
+        )).id();
+        let speed_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Speed: {}", speed.0)),
+        )).id();
+        let corruption_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Corruption: {}", corruption.0)),
+        )).id();
+        let matter_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Matter: {}", matter.0)),
+        )).id();
+        let mind_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Mind: {}", mind.0)),
+        )).id();
+        let luck_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Luck: {}", luck.0)),
+        )).id();
+        let maneuver_node = commands.spawn((
+            Node {
+                ..default()
+            },
+            Text::new(format!("Maneuver: {}", maneuver.0)),
+        )).id();
         commands.spawn((
-                floating_window_root("EQUIPT TEST".to_string(),
+                floating_window_root("Stats".to_string(),
                     (
                         Node {
                             flex_grow: 1.,
@@ -41,64 +89,16 @@ pub fn display_stats_event_observer(
                             overflow: Overflow { x: bevy::ui::OverflowAxis::Hidden, y: bevy::ui::OverflowAxis::Hidden },
                             ..default()
                         },
-                        Children::spawn(SpawnWith(|parent: &mut ChildSpawner| {
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new(format!("Level")),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Experience"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Speed"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Corruption"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Matter"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Mind"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Luck"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Maneuver"),
-                            ));
-                            parent.spawn((
-                                    Node {
-                                        ..default()
-                                    },
-                                    Text::new("Hunger"),
-                            ));
-                        })),
                     ),
                     ),
-        ));
+        ))
+        .add_child(level_node)
+        .add_child(experience_node)
+        .add_child(speed_node)
+        .add_child(corruption_node)
+        .add_child(matter_node)
+        .add_child(mind_node)
+        .add_child(luck_node)
+        .add_child(maneuver_node);
     }
 }
