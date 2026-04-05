61b28d7856368084ed8fc0f522059d74a8d4cf7f
Drake <fyiohhai@gmail.com>
2026-04-05 11:38:39 -0400

    convert chair to new camera interpolation system

--- src/furniture/chair.rs
+++ src/furniture/chair.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use avian3d::prelude::RigidBodyDisabled;
 use bevy::{ecs::{lifecycle::HookContext, world::DeferredWorld}, prelude::*};
 
-use crate::{CameraInterpolation, Interactable, InteractionEvent, Player, PlayerCamera, PlayerState};
+use crate::{CameraInterpolation, CameraInterpolation2, Interactable, InteractionEvent, Player, PlayerCamera, PlayerState};
 
 #[derive(Debug, Default, Component, Reflect)]
 #[reflect(Component)]
@@ -37,24 +37,26 @@ fn chair_interaction_observer(
     mut commands: Commands,
     mut player_query: Query<(&mut Transform, &mut PlayerState, Entity), With<Player>>,
     transform_query: Query<&GlobalTransform, Without<Player>>,
-    camera_query: Query<Entity, With<PlayerCamera>>,
+    camera_query: Query<(Entity, &Transform), (With<PlayerCamera>, Without<Player>)>,
     time: Res<Time>,
 ) {
     trace!("OBSERVER: chair_interaction_observer");
     if let Ok((mut player_transform, mut player_state, player_entity)) = player_query.single_mut()
         && let Ok(chair_transform) = transform_query.get(trigger.entity)
-        && let Ok(camera_entity) = camera_query.single() {
+        && let Ok((camera_entity, camera_transform)) = camera_query.single() {
             *player_transform = Transform {
                 translation: Vec3 { x: chair_transform.translation().x, y: chair_transform.translation().y + 1.0, z: chair_transform.translation().z },
                 rotation: chair_transform.rotation(),
                 ..default()
             };
             *player_state = PlayerState::Sitting;
-            commands.entity(player_entity).insert(RigidBodyDisabled);
+            //commands.entity(player_entity).insert(RigidBodyDisabled);
             commands.entity(camera_entity)
-                .insert(CameraInterpolation {
+                .insert(CameraInterpolation2 {
                     duration: time.elapsed() + Duration::new(1, 0),
                     start_time: time.elapsed(),
+                    start_pos: *camera_transform,
+                    desired_pos: *player_transform,
                 });
     }
 }
