1234d5abb66b3a69ed27563bcd5a87270db9766d
Drake <fyiohhai@gmail.com>
2026-04-05 11:39:02 -0400

    convert bed to new camera interpolation system

--- src/furniture/bed.rs
+++ src/furniture/bed.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use avian3d::prelude::RigidBodyDisabled;
 use bevy::{ecs::{lifecycle::HookContext, world::DeferredWorld}, prelude::*};
 
-use crate::{CameraInterpolation, InteractionEvent, Player, PlayerCamera, PlayerState};
+use crate::{CameraInterpolation, CameraInterpolation2, InteractionEvent, Player, PlayerCamera, PlayerState};
 
 #[derive(Component, Reflect)]
 #[reflect(Component)]
@@ -25,13 +25,13 @@ fn bed_interaction_observer(
     mut commands: Commands,
     bed_query: Query<&GlobalTransform, With<Bed>>,
     mut player_query: Query<(Entity, &mut Transform, &mut PlayerState), With<Player>>,
-    camera_query: Query<Entity, With<PlayerCamera>>,
+    camera_query: Query<(Entity, &Transform), (With<PlayerCamera>, Without<Player>)>,
     time: Res<Time>,
 ) {
     trace!("OBSERVER: bed_interaction_observer");
     if let Ok(bed_global_transform) = bed_query.get(trigger.entity)
     && let Ok((player_entity, mut player_transform, mut player_state)) = player_query.single_mut()
-    && let Ok(camera_entity) = camera_query.single() {
+    && let Ok((camera_entity, camera_transform)) = camera_query.single() {
         *player_transform = Transform {
             translation: Vec3 {
                 x: bed_global_transform.translation().x,
@@ -42,11 +42,13 @@ fn bed_interaction_observer(
             ..default()
         };
         *player_state = PlayerState::Sleeping;
-        commands.entity(player_entity).insert(RigidBodyDisabled);
+        //commands.entity(player_entity).insert(RigidBodyDisabled);
         commands.entity(camera_entity)
-            .insert(CameraInterpolation {
+            .insert(CameraInterpolation2 {
                 duration: time.elapsed() + Duration::new(1, 0),
                 start_time: time.elapsed(),
+                start_pos: *camera_transform,
+                desired_pos: *player_transform,
             });
     }
 }
