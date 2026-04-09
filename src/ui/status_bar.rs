cdbf49c6820d0ea5a857ece9e0928850e2fc412e
Drake <fyiohhai@gmail.com>
2026-04-08 21:24:46 -0400

    Added sleep to status bar

--- src/ui/status_bar.rs
+++ src/ui/status_bar.rs
@@ -6,6 +6,31 @@ const HEALTH_SHADER_ASSET_PATH: &str = "shaders/health_ui_material.wgsl";
 const MANA_SHADER_ASSET_PATH: &str = "shaders/mana_ui_material.wgsl";
 
 #[derive(AsBindGroup, Asset, TypePath, Debug, Clone)]
+struct SleepUiMaterial {
+    /// Color multiplied with the image
+    #[uniform(0)]
+    color: Vec4,
+    /// Represents how much of the image is visible
+    /// Goes from 0 to 1
+    /// A `Vec4` is used here because Bevy with webgl2 requires that uniforms are 16-byte aligned but only the first component is read.
+    #[uniform(1)]
+    slider: Vec4,
+    /// Image used to represent the slider
+    #[texture(2)]
+    #[sampler(3)]
+    color_texture: Handle<Image>,
+    /// Color of the image's border
+    #[uniform(4)]
+    border_color: Vec4,
+}
+
+impl UiMaterial for SleepUiMaterial {
+    fn fragment_shader() -> bevy::shader::ShaderRef {
+        HEALTH_SHADER_ASSET_PATH.into()
+    }
+}
+
+#[derive(AsBindGroup, Asset, TypePath, Debug, Clone)]
 struct HealthUiMaterial {
     /// Color multiplied with the image
     #[uniform(0)]
@@ -94,16 +119,61 @@ pub struct ManaUiNode;
 #[component(on_add = on_health_ui_node_add)]
 pub struct HealthUiNode;
 
+#[derive(Component, Reflect)]
+#[require(
+    Node {
+        ..default()
+    },
+    Button,
+    Text("Player Sleep".to_string()),
+    TextColor(Color::WHITE),
+    ZIndex(10),
+)]
+#[component(on_add = on_sleep_ui_node_add)]
+pub struct SleepUiNode;
+
 pub struct StatusUIPlugin;
 impl Plugin for StatusUIPlugin {
     fn build(&self, app: &mut App) {
        app
            .add_plugins(UiMaterialPlugin::<HealthUiMaterial>::default())
            .add_plugins(UiMaterialPlugin::<ManaUiMaterial>::default())
+           .add_plugins(UiMaterialPlugin::<SleepUiMaterial>::default())
            .add_systems(OnEnter(GameState::Gameplay), draw_status_ui)
            .add_systems(Update, animate_health_material)
-           .add_systems(Update, animate_mana_material);
+           .add_systems(Update, animate_mana_material)
+           .add_systems(Update, animate_sleep_material);
     }
+}
+
+fn on_sleep_ui_node_add(
+    mut world: DeferredWorld,
+    context: HookContext,
+) {
+    let asset_server = world.resource::<AssetServer>();
+    let font = asset_server.load("FiraSans-Bold.ttf");
+
+    let ui_assets = world.resource::<DAUiAssets>();
+    let image = ui_assets.health_1.clone();
+
+    let mut ui_materials = world.resource_mut::<Assets<SleepUiMaterial>>();
+    let material = ui_materials.add(SleepUiMaterial {
+        color: LinearRgba::WHITE.to_f32_array().into(),
+        slider: Vec4::splat(0.5),
+        color_texture: image,
+        border_color: LinearRgba::WHITE.to_f32_array().into(),
+    });
+
+    world.commands()
+        .entity(context.entity)
+        .insert(TextFont {
+            font,
+            font_size: 50.0,
+            ..default()
+        })
+        .insert((
+                MaterialNode(material),
+        ));
 }
 
 fn on_health_ui_node_add(
@@ -201,8 +271,38 @@ pub fn draw_status_ui(
                 ))
                 .id();
 
+            let player_sleep_node = commands
+                .spawn((
+                    SleepUiNode,
+                ))
+                .id();
+
             commands.entity(status_bar_node).add_child(player_health_node);
             commands.entity(status_bar_node).add_child(player_mana_node);
+            commands.entity(status_bar_node).add_child(player_sleep_node);
+    }
+}
+
+fn animate_sleep_material(
+    mut materials: ResMut<Assets<SleepUiMaterial>>,
+    query: Query<&MaterialNode<SleepUiMaterial>>,
+    sleep_query: Query<&Sleep, With<Player>>,
+    time: Res<Time>,
+) {
+    //let duration = 2.0;
+    for handle in &query {
+        if let Some(material) = materials.get_mut(handle)
+        && let Ok(sleep) = sleep_query.single() {
+            let value = sleep.value as f32 / 100.0;
+            // rainbow color effect
+            let new_color = Color::hsl((time.elapsed_secs() * 60.0) % 360.0, 1., 0.5);
+            let border_color = Color::hsl((time.elapsed_secs() * 60.0) % 360.0, 0.75, 0.75);
+            material.color = new_color.to_linear().to_vec4();
+            material.slider.x =
+                //((time.elapsed_secs() % (duration * 2.0)) - duration).abs() / duration;
+                value;
+            material.border_color = border_color.to_linear().to_vec4();
+        }
     }
 }
 
