0d29bf94196ed875ab7442ab0f782a4b7ea172be
Drake <fyiohhai@gmail.com>
2026-04-08 21:21:26 -0400

    more work on lights

--- src/furniture/ceiling_light.rs
+++ src/furniture/ceiling_light.rs
@@ -1,4 +1,4 @@
-use bevy::{ecs::{lifecycle::HookContext, world::DeferredWorld}, prelude::*};
+use bevy::{ecs::{lifecycle::HookContext, world::DeferredWorld}, input::common_conditions::input_just_pressed, prelude::*};
 
 #[derive(Component, Reflect)]
 #[reflect(Component)]
@@ -19,6 +19,20 @@ pub struct CeilingLightPlugin;
 impl Plugin for CeilingLightPlugin {
     fn build(&self, app: &mut App) {
         app
-            .register_type::<LightFlicker>();
+            .register_type::<LightFlicker>()
+            .add_systems(Update, toggle_light.run_if(input_just_pressed(KeyCode::KeyX)));
+    }
+}
+
+fn toggle_light(
+    mut commands: Commands,
+    light_query: Query<(Entity, Option<&PointLight>), With<LightFlicker>>,
+) {
+    if let Ok((light_entity, point_light)) = light_query.single() {
+        if point_light.is_some() {
+            commands.entity(light_entity).remove::<PointLight>();
+        } else {
+            commands.entity(light_entity).insert(PointLight::default());
+        }
     }
 }
