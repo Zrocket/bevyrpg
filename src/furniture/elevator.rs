3c68ad83e0460c766909d6eefa5f6dd6e7cd2497
Drake <fyiohhai@gmail.com>
2026-07-17 20:24:32 -0400

    elevator finish

--- src/furniture/elevator.rs
+++ src/furniture/elevator.rs
@@ -1,4 +1,4 @@
-use bevy::{ecs::{lifecycle::HookContext, world::DeferredWorld}, mesh::VertexAttributeValues, prelude::*};
+use bevy::{ecs::{lifecycle::HookContext, world::DeferredWorld}, math::Affine3A, mesh::VertexAttributeValues, prelude::*};
 
 #[derive(Component, Reflect)]
 #[reflect(Component)]
@@ -43,39 +43,41 @@ impl Plugin for ElevatorPlugin {
 
 fn elevator_button_interaction_observer(
     _trigger: On<crate::InteractionEvent>,
+    time: Res<Time>,
     meshes: Res<Assets<Mesh>>,
-    mut elevator_query: Query<(Entity, &mut Elevator, &mut Transform)>,
+    mut elevator_query: Query<(Entity, &mut Elevator, &mut Transform, &GlobalTransform), Without<ElevatorCurve>>,
     curve_mesh_query: Query<(&Mesh3d, &GlobalTransform), With<ElevatorCurve>>,
 ) {
-    if let Ok((entity, mut elevator, mut elevator_transform)) = elevator_query.single_mut()
+    if let Ok((entity, mut elevator, mut elevator_transform, elevator_global_transform)) = elevator_query.single_mut()
     && let Ok((curve_mesh3d, curve_global_transform)) = curve_mesh_query.single()
     && let Some(mesh) = meshes.get(&curve_mesh3d.0)
     && let Some(VertexAttributeValues::Float32x3(positions)) = mesh.attribute(Mesh::ATTRIBUTE_POSITION) {
+        let ease_function = EaseFunction::SmoothStep;
+        let scale = elevator_transform.scale;
+        let rotation = elevator_transform.rotation;
         if let Some(_current_point) = positions.get(elevator.current) {
             elevator.current += 1;
             if let Some(next_point) = positions.get(elevator.current) {
-                let world_position = curve_global_transform.transform_point(Vec3::from(*next_point));
+                let point_vec = vec3(next_point[0], next_point[1], next_point[2]);
+
                 *elevator_transform = Transform {
-                    translation:  world_position,
-                    rotation: elevator_transform.rotation,
-                    scale: elevator_transform.scale,
-                }
+                    translation:  point_vec,
+                    rotation,
+                    scale,
+                };
             } else {
                 elevator.current = 0;
                 if let Some(next_point) = positions.get(elevator.current) {
-                    let world_position = curve_global_transform.transform_point(Vec3::from(*next_point));
+                    let point_vec = vec3(next_point[0], next_point[1], next_point[2]);
+                    *elevator_transform = elevator_global_transform.reparented_to(curve_global_transform);
+
                     *elevator_transform = Transform {
-                        translation:  world_position,
-                        rotation: elevator_transform.rotation,
-                        scale: elevator_transform.scale,
-                    }
+                        translation:  point_vec,
+                        rotation,
+                        scale,
+                    };
                 }
             }
-        }
-
-        for &position in positions {
-            let world_position = curve_global_transform.transform_point(Vec3::from(position));
-            println!("{:?}", world_position);
         }
     }
 }
